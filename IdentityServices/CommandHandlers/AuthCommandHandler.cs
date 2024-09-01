f24e15f77fd3a5c07005d46f29dbc3d411e54934
sstacker_m <77928053+meenrock@users.noreply.github.com>
2024-09-01 13:54:25 +0700

    sdas

---
+++ IdentityServices/CommandHandlers/AuthCommandHandler.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Options;
+
+namespace IdentityServices.CommandHandlers;
+
+public class AuthCommandHandler
+{
+    private readonly IOptionsSnapshot<>
+    
+    public AuthCommandHandler()
+    {
+        
+    }
+}
